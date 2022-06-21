a04d03322d4bdbb7da392019a0d825456db5a228
Alvin Chinedu <therealalv@gmail.com>
2022-06-21 20:40:31 +0100

    tempereture converter

--- JavaScript/Week2/Arrays/arrays.js
+++ JavaScript/Week2/Arrays/arrays.js
@@ -1,26 +1,40 @@
-let animals = [ 
-    {name: 'dog', breed: 'mammal'}, 
-    {name: 'goat', breed: 'mammal'},
-    {name: 'snake', breed: 'reptile'},
-    {name: 'frog', breed: 'amphibian'}
-]
+// let animals = [ 
+//     {name: 'dog', breed: 'mammal'}, 
+//     {name: 'goat', breed: 'mammal'},
+//     {name: 'snake', breed: 'reptile'},
+//     {name: 'frog', breed: 'amphibian'}
+// ]
 
 
-for (let animal in animals) {
-    console.log(`The animal name is ${Object.values(animals[animal])[0]} and the breed is ${Object.values(animals[animal])[1]}`)
-}
+// for (let animal in animals) {
+//     console.log(`The animal name is ${Object.values(animals[animal])[0]} and the breed is ${Object.values(animals[animal])[1]}`)
+// }
 
 
 
-function addAnimal(name, breed) {
-    alert("Add an animal to the database")
+// function addAnimal(name, breed) {
+//     alert("Add an animal to the database")
    
-    let animalDetails = {}
-    animalDetails['name'] = prompt("Enter Animal Name")
-    animalDetails['breed']= prompt('Enter Animal Breed')
+//     let animalDetails = {}
+//     animalDetails['name'] = prompt("Enter Animal Name")
+//     animalDetails['breed']= prompt('Enter Animal Breed')
     
-    animals.push(animalDetails)
-    console.log(animals)
-}
+//     animals.push(animalDetails)
+//     console.log(animals)
+// }
 
-addAnimal()
+// addAnimal()
+
+alert('Welcome to the tempreture converter')
+
+let defaultTemp = 45
+let convert = prompt(`The tempereture for today is ${defaultTemp} degrees\nType CEL to convert to Celcuis and KEL to convert to Kelvin`)
+
+if(convert == 'CEL') {
+    let celcuis = (defaultTemp - 32) * 5/9
+    alert(`The tempereture in celcuis is ${celcuis} degrees`)
+} 
+else if (convert == 'KEL') {
+    let kelvin = (defaultTemp + 459.67) * 5/9
+    alert(`The tempereture in kelvin is ${kelvin} degrees`)
+}
