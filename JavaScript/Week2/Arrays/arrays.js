87eae7dac8c20ec4fb8b907e8046f6297d68583a
Alvin Chinedu <therealalv@gmail.com>
2022-06-25 23:53:36 +0100

    destructuring in js

--- JavaScript/Week2/Arrays/arrays.js
+++ JavaScript/Week2/Arrays/arrays.js
@@ -25,16 +25,66 @@
 
 // addAnimal()
 
-alert('Welcome to the tempreture converter')
+// alert('Welcome to the tempreture converter')
 
-let defaultTemp = 45
-let convert = prompt(`The tempereture for today is ${defaultTemp} degrees\nType CEL to convert to Celcuis and KEL to convert to Kelvin`)
+// let defaultTemp = 45
+// let convert = prompt(`The tempereture for today is ${defaultTemp} degrees\nType CEL to convert to Celcuis and KEL to convert to Kelvin`)
 
-if(convert == 'CEL') {
-    let celcuis = (defaultTemp - 32) * 5/9
-    alert(`The tempereture in celcuis is ${celcuis} degrees`)
-} 
-else if (convert == 'KEL') {
-    let kelvin = (defaultTemp + 459.67) * 5/9
-    alert(`The tempereture in kelvin is ${kelvin} degrees`)
+// if(convert == 'CEL') {
+//     let celcuis = (defaultTemp - 32) * 5/9
+//     alert(`The tempereture in celcuis is ${celcuis} degrees`)
+// } 
+// else if (convert == 'KEL') {
+//     let kelvin = (defaultTemp + 459.67) * 5/9
+//     alert(`The tempereture in kelvin is ${kelvin} degrees`)
+// }
+
+// let isGuestOneVegan = false
+// let isGuestTwoVegan = false
+
+// if(isGuestOneVegan && isGuestTwoVegan) {
+//     console.log('Both are vegan')
+// } else if (isGuestOneVegan || isGuestTwoVegan) {
+//     console.log('One is vegan')
+// } else if (!isGuestOneVegan && !isGuestTwoVegan) {
+//     console.log('None is vegan')
+// }
+
+// function tipCalculator (tipPercent = 0.2) {
+//     let total = prompt('Enter your total bill amount')
+
+//     let result = total * tipPercent
+//     return alert(`Your tip ampount is: ${result}`)
+// }
+
+// tipCalculator(0.4) 
+
+const profileMap = new Map()
+
+profileMap.set('State', "Anambra")
+profileMap.set('Age', 23)
+profileMap.set('Name', "Okafor Alvin")
+
+console.log(profileMap)
+
+let arrayMap = Array.from(profileMap)
+console.log(arrayMap)
+
+let objectMap = Object.fromEntries(profileMap)
+console.log(objectMap)
+
+
+const user = {
+    fullname: 'Alvin Chinedu Okafor',
+    username: 'alvinchinedu',
+    email: 'alvin2k99@gmail.com',
+    password: 'okviofcn00643czl'
 }
+
+let getUserName = ({fullname, username, email, password}) => {
+    console.log(`The fullname is ${fullname}`)
+    console.log(`The username is ${username}`)
+    console.log(`The email is ${email}`)
+}
+
+getUserName(user)
